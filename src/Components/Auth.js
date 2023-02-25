e2371442363dd8a97486365514de94434db74cda
Gaurav Sonawane <vgauravs2001@gmail.com>
2023-02-25 13:45:10 +0530

    Initiate project, create empty authentication components

---
+++ src/Components/Auth.js
@@ -0,0 +1,13 @@
+import Login from "./Login";
+import SignUp from "./SignUp";
+const Auth = () => {
+    return (
+        <div>
+            Hello world
+            <SignUp/>
+            <Login/>
+        </div>
+    );
+}
+
+export default Auth;
