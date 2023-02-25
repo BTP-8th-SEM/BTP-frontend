04d3b65939872711677427b9bd948a965a34c805
Gaurav Sonawane <vgauravs2001@gmail.com>
2023-02-25 14:06:26 +0530

    clean up default code

--- src/App.js
+++ src/App.js
@@ -2,26 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 import Auth from './Components/Auth';
+import SignUp from './Components/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          
-        </a>
-        <Auth/>
-      </header>
+      // write your code here.
     </div>
   );
 }
