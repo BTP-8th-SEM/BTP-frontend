c960581c0ba78bd4124d2fc25d302071c5ea0999
Gaurav Sonawane <vgauravs2001@gmail.com>
2023-02-26 12:46:33 +0530

    add logic for taking input

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,23 +1,49 @@
+import React, { useState } from 'react';
 import './../Styles/login.css';
 const Login = () => {
+    const [emailVal, setEmailVal] = useState('');
+    const [passVal, setPassVal] = useState('');
+    const handleEmailInputChange = (e) => {
+        setEmailVal(e.target.value);
+    }
+    const handlePassInputChange = (e) => {
+        
+        setPassVal(e.target.value);
+    }
+    const hitSubmit = (e) => {
+        e.preventDefault();
+        alert(`${emailVal}, ${passVal}`)
+    }
+
     return (
         <div className="login-box">
             <h2> Login </h2>
-            <div class="login-form">
-                <div className="mb-3">
-                    <label className="form-label" id="email-l">Email</label>
-                    <input type="text" className="form-control" id="email-i" placeholder="name@example.com"/>
-                </div>
-                <div className="mb-3">
-                    <label className="form-label" id="pass-l">Password</label>
-                    <input type="password" className="form-control" id="pass-i" placeholder="Password"/>
-                </div>
-                
-                <button type="button" className="btn btn-success" id="s">Submit</button>
+            <form onSubmit={hitSubmit}>
+                <div class="login-form">
+                    <div className="mb-3">
+                        <label className="form-label" id="email-l">Email</label>
+                        <input type="text"
+                            className="form-control"
+                            id="email-i"
+                            placeholder="name@example.com"
+                            onChange={handleEmailInputChange} />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label" id="pass-l">Password</label>
+                        <input type="password"
+                            className="form-control"
+                            id="pass-i"
+                            placeholder="Password"
+                            onChange={handlePassInputChange} />
+                    </div>
 
+                    <button type="submit"
+                        className="btn btn-success"
+                        id="s">Submit</button>
 
-                
-            </div>
+                </div>
+            </form>
+
         </div>
     );
 }
