bce8616bebb28c3c203f3382525b1f24d96fbf9a
Gaurav Sonawane <vgauravs2001@gmail.com>
2023-03-04 01:44:57 +0530

    add app name instead of Navbar in Navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,7 +2,7 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">InTest</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
