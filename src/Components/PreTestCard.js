b54c0d9c86cd47bc44acf8741154d087bafe9128
Gaurav Sonawane <vgauravs2001@gmail.com>
2023-04-26 21:23:25 +0530

    changes 1

--- src/Components/PreTestCard.js
+++ src/Components/PreTestCard.js
@@ -6,7 +6,7 @@ const PreTestCard = (props) => {
                     <h5 className="card-title">{props.test["name"]}</h5>
                     
                     <h6>Maximum Marks : {props.test["maxMarks"]}</h6>
-                    <h6>Your Marks : {props.test["marks"]}</h6>
+                    <h6>Your Marks : {props.test["obtainedMarks"]}</h6>
                     {/* <p className="card-text">With supporting text below as a natural lead-in to additional content.</p> */}
                     <button className="btn btn-light">See Analytics</button>
                     
